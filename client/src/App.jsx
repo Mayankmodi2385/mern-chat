6cea7cda77125dc616ec6e43c88e113a413d9cc0
Mayankmodi2385 <mayankmodi7126@gmail.com>
2026-04-17 09:49:09 +0530

    remove localhost completely

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import {UserContextProvider} from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = 'http://localhost:4000';
+  axios.defaults.baseURL = 'http://mern-chat-backend-g0fl.onrender.com';
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
