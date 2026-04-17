c7164fb840eea7f1a3b6ddbbe9111b34dd545084
Mayankmodi2385 <mayankmodi7126@gmail.com>
2026-04-17 12:28:40 +0530

    fix crash

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -12,7 +12,7 @@ export default function Avatar({userId, username, online}) {
 
   return (
     <div className={"w-8 h-8 relative rounded-full flex items-center "+color}>
-      <div className="text-center w-full opacity-70">{username[0]}</div>
+      <div className="text-center w-full opacity-70">{username ? username[0] : ''}</div>
       {online && (
         <div className="absolute w-3 h-3 bg-green-400 bottom-0 right-0 rounded-full border border-white"></div>
       )}
